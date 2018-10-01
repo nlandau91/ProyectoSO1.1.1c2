f94c09d6ea4ef5a4a59e975d82fb4898e7e474f0
nlandau91 <43214457+nlandau91@users.noreply.github.com>
2018-09-30 23:51:22 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -125,7 +125,7 @@
         FILE *fp;
         fp=fopen("sudoku.txt","r");
         if(fp==NULL){
-			printf("No se pudo abrir el archivo sudoku.txt\n");
+            printf("No se pudo abrir el archivo sudoku.txt\n");
             exit(EXIT_FAILURE);
         }
         int i,j;
